20dabdce5bf0139d787542491010c28472f99332
Eamon Woortman <eamon.woortman@gmail.com>
2024-01-20 17:11:04 +0100

    Added Alt+L hotkey to open ShaderAmp

--- src/backgroundPage.ts
+++ src/backgroundPage.ts
@@ -1,7 +1,7 @@
 import {getStorage, setStorage} from "@src/helpers/storage";
 import browser from "webextension-polyfill";
 import {START} from "@src/helpers/constants";
-import {tabStreamCapture} from "@src/helpers/tabActions";
+import {doesTabExist, getCurrentTab, tabStreamCapture} from "@src/helpers/tabActions";
 
 const removeTab = (tabId: number) => {
     return new Promise((resolve) => {
@@ -9,20 +9,45 @@ const removeTab = (tabId: number) => {
     });
 }
 
+const openShaderAmp = async (openerTabId: number | undefined) => {
+    // Fetch the current tab id in case it's not passed as a parameter
+    if (openerTabId === undefined) {
+        const currentTab = await getCurrentTab();
+        openerTabId = currentTab?.id!;
+    }
+    // Check if the content tab is not already open
+    const openTabs: TabMapping = await getStorage('tabMapping')
+    if (openTabs) {
+        const alreadyOpened = openTabs[openerTabId]
+        if (alreadyOpened && await doesTabExist(alreadyOpened.contentTabId)) {
+            // jump to the already opened tab
+            await browser.tabs.update(alreadyOpened.contentTabId, {active: true})
+            return Promise.resolve();
+        }
+    }
+
+    // Create a new content tab
+    const targetTab = await browser.tabs.create({url: 'content.html', openerTabId: openerTabId, active: false});
+    const stream =  await tabStreamCapture(openerTabId, targetTab.id as number);
+
+    // Cache the content tab id and target stream
+    const tabMapping : TabMapping = {};
+    tabMapping[openerTabId] = {
+        contentTabId: targetTab.id as number,
+        stream,
+    };
+    await setStorage("tabMapping", tabMapping);
+    // Set the new content tab active
+    await browser.tabs.update(targetTab.id as number, {active: true});
+
+    return Promise.resolve();
+}
+
 browser.runtime.onMessage.addListener(async (msg, sender) => {
     if (msg.command && (msg.command === START)) {
-        const targetTab = await browser.tabs.create({url: 'content.html', openerTabId: msg.openerTabId, active: false});
-        const stream =  await tabStreamCapture(msg.openerTabId as number, targetTab.id as number);
-
-        const tabMapping : TabMapping = {};
-        tabMapping[msg.openerTabId as number] = {
-            contentTabId: targetTab.id as number,
-            stream,
-        };
-        await setStorage("tabMapping", tabMapping);
-        await browser.tabs.update(targetTab.id as number, {active: true});
-        return Promise.resolve();
+        await openShaderAmp(msg.openerTabId);
     }
+    return Promise.resolve();
 });
 
 browser.tabs.onRemoved.addListener(async (tabId) => {
@@ -33,4 +58,11 @@ browser.tabs.onRemoved.addListener(async (tabId) => {
         delete tabMapping[tabId];
         await setStorage("tabMapping", tabMapping)
     }
+});
+
+browser.commands.onCommand.addListener(async (command) => {
+    if (command !== 'open-shader-amp') {
+        return Promise.resolve();
+    }
+    await openShaderAmp(undefined);
 });
