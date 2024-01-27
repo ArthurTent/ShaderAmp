3477e6050c17481037c9226adbcc393bf71aeebf
Eamon Woortman <eamon.woortman@gmail.com>
2024-01-27 11:52:07 +0100

    Added pre-dev/build step to build a shader list

---
+++ src/buildShaderList.js
@@ -0,0 +1,42 @@
+/* eslint no-param-reassign: 0, no-console: 0 */
+const fs = require('fs');
+const path = require('path');
+
+function isShaderFile(file) {
+    return path.extname(file) == '.frag';
+}
+// return an ordered list of files in the input dir, with full paths
+function listFilesSync(dir) {
+  let fileList = [];
+  fs.readdirSync(dir).forEach((file) => {
+    const fullPath = path.join(dir, file);
+    // use lstat so this does not follow dir symlinks
+    // (otherwise this will include files from other dirs, which I don't want)
+    if (fs.lstatSync(fullPath).isDirectory()) {
+      fileList = fileList.concat(listFilesSync(fullPath));
+    } else if (isShaderFile(fullPath)) {
+      fileList.push(fullPath);
+    }
+  });
+  return fileList;
+}
+
+// return an object with the file path and file size
+function formatFileInfo(file) {
+    const fileName = path.basename(file);
+    return fileName;
+}
+
+const directory = path.join(__dirname, '../dist/shaders');
+const fullDirPath = path.resolve(directory);
+console.log(`Processing dir '${fullDirPath}'...`);
+
+const fullFileList = listFilesSync(fullDirPath);
+console.log(`Found ${fullFileList.length} files`);
+
+const formattedFiles = fullFileList.map(formatFileInfo);
+
+const outFile = `${directory}/list.json`;
+fs.writeFileSync(outFile, JSON.stringify(formattedFiles, null, 2));
+console.log(`Wrote file ${outFile}`);
+
