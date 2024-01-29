58945aba8cc22238259514e5c1d0649b593c0363
Eamon Woortman <eamon.woortman@gmail.com>
2024-01-29 19:04:50 +0100

    Added toggle to toggle preview in the options tab

--- src/helpers/optionsActions.ts
+++ src/helpers/optionsActions.ts
@@ -1,14 +1,12 @@
 import { tabStreamCapture } from '@src/helpers/tabActions';
 import { getAppState } from '@src/helpers/tabMappingService';
 
-export const acquireVideoStream = async (video : HTMLVideoElement) => {
+export const acquireVideoStream = async (video : HTMLVideoElement): Promise<MediaStream | undefined> => {
     const appState = await getAppState()
     const optionsTab = appState.optionsTab;
     const contentTabId = optionsTab.contentTabId;
     if (contentTabId) {
-        console.log(`contentTabId: ${contentTabId}`);
         const streamId = await tabStreamCapture(contentTabId, optionsTab.tabId);
-
         const stream = await navigator.mediaDevices.getUserMedia({
             audio: false,
             video: {
@@ -21,5 +19,7 @@ export const acquireVideoStream = async (video : HTMLVideoElement) => {
         });
         console.log(`streamId: ${stream}`);
         video.srcObject = stream;
+
+        return stream;
     }
 }
--- src/options/Options.tsx
+++ src/options/Options.tsx
@@ -1,6 +1,6 @@
 import browser, { Tabs } from "webextension-polyfill";
 import { acquireVideoStream } from '@src/helpers/optionsActions';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { getAppState, getTabMappings } from "@src/helpers/tabMappingService";
 import { SPACE } from "@src/helpers/constants";
 import { doesTabExist, getCurrentTab, tabStreamCapture } from "@src/helpers/tabActions";
@@ -10,28 +10,54 @@ import "./styles.module.css";
 
 
 const Options: React.FC = () => {
+    const [showVideoPreview, setShowVideoPreview] = useState<boolean>(false);
     const videoElement = useRef<HTMLVideoElement>(null);
+    const [videoStream, setVideoStream] = useState<MediaStream|undefined>();
+
+    const [shaderName, setShaderName] = useState<string>('MusicalHeart.frag');
+    const [shaderList, setShaderList] = useState<string[]>([]);
+    const [shaderIndex, setShaderIndex] = useState<number>(0);
+
+    const handleShowPreviewInput = (event:any) => {
+        event.preventDefault();
+        setShowVideoPreview(!showVideoPreview);
+    }
+
+    const setupVideoStream = async () => {
+        const stream = await acquireVideoStream(videoElement.current as HTMLVideoElement);
+        setVideoStream(stream);
+    }
+
+    const shutDownVideoStream = async () => {
+        if (!videoStream) {
+            return;
+        }
+        const videoTrack = videoStream.getTracks()[0];
+        videoTrack?.stop();
+    }
 
     useEffect(() => {
-        acquireVideoStream(videoElement.current as HTMLVideoElement);
-    }, []);
+        if (showVideoPreview) {
+            setupVideoStream();
+        } else {
+            shutDownVideoStream();
+        }
+    }, [showVideoPreview]);
 
     const sendTestMessage = async() => {
-        const appState = await getAppState();
-        const optionsTabId = appState.optionsTab?.tabId;
-        const isOptionsTabOpen = optionsTabId && await doesTabExist(optionsTabId);
-
         const currentTab = await getCurrentTab();
-        const openTabs: TabMapping = await getTabMappings();
         browser.runtime.sendMessage({ command: SPACE, openerTabId: currentTab?.id });
-
     }
 
     return (
         <div className="flex items-center flex-col p-5 w-screen	h-screen bg-white dark:bg-gray-900 antialiased">
             <h2 className="text-4xl font-extrabold dark:text-white">ShaderAmp Options Page</h2>
-            <p className="my-4 text-lg text-gray-500">Preview</p>
-            <video ref={videoElement} className="max-w-96 max-h-96 rounded-lg" playsInline autoPlay muted/>
+            <label className="my-4 relative inline-flex items-center cursor-pointer">
+                <input type="checkbox" onInput={handleShowPreviewInput} value={showVideoPreview ? 1 : 0} className="sr-only peer"/>
+                <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Show Preview (experimental)</span>
+            </label>
+            {showVideoPreview && <><p className="my-4 text-lg text-gray-500">Preview</p><video ref={videoElement} className="max-w-96 max-h-96 rounded-lg" playsInline autoPlay muted/></>}
             <p className="my-4 text-lg text-gray-500">Actions</p>
             <div className="flex flex-wrap">
                 <button className="h-10 px-5 m-2 text-white font-medium transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800"
