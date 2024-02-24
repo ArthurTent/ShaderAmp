e057fb6e3f6817e1cd02d9f361c397e9bd386163
Eamon Woortman <eamon.woortman@gmail.com>
2024-02-24 14:12:27 +0100

    Added wip types setup

--- src/helpers/types.ts
+++ src/helpers/types.ts
@@ -17,6 +17,21 @@ type AppState = {
     optionsTab: OptionsTab;
 } 
 
+type ShaderResourceType = "webcam" | "video" | "texture";
+
+type ShaderResource = { 
+    type: ShaderResourceType;
+    // ... no default properties
+}
+
+class WebcamShaderResource implements ShaderResource {
+    type: ShaderResourceType = "webcam";
+}
+
+class VideoShaderResource implements ShaderResource {
+    type: ShaderResourceType = "video";
+}
+
 type ShaderObject = {
     shaderName: string;
     metaData?: any;
@@ -27,3 +42,20 @@ type ShaderCatalog = {
     lastModified: Date;
 }
 
+/*
+// move to loadFragmentShader func
+const loadShader = (shaderObject : ShaderObject) => {
+    const resources:ShaderResource[] = shaderObject.metaData?.resources;
+    if (resources) {
+        resources.forEach(function (resource) {
+            switch (resource.type) {
+                case "webcam":
+                    loadWebCam();
+                    putWebcamResourceInUniform();
+                case "video":
+                case "texture":
+            }
+        });
+    }
+}
+*/
